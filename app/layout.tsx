00c011c94657197c478f81865c6d29e5747d81ab
iampandaro <matei.florentin.cucu@cneminescu.ro>
2026-04-22 17:12:56 +0300

    feat(layout): updated font variations

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,14 +8,94 @@ import Navbar from "@/components/navbar";
 const chandlerMountain = localFont({
   src: [
     {
+      path: "../public/fonts/ChandlerMountain-Thin-iF66cbd28d76d6c.ttf",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/ChandlerMountain-ThinSlanted-iF66cbd28d85c4f.ttf",
+      weight: "100",
+      style: "italic",
+    },
+    {
+      path: "../public/fonts/ChandlerMountain-ExtraLight-iF66cbd1d2c1553.ttf",
+      weight: "200",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/ChandlerMountain-ExtLtSlant-iF66cbd1d29eeac.ttf",
+      weight: "200",
+      style: "italic",
+    },
+    {
+      path: "../public/fonts/ChandlerMountain-Light-iF66cbd244a3616.ttf",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/ChandlerMountain-LightSlanted-iF66cbd244b8762.ttf",
+      weight: "300",
+      style: "italic",
+    },
+    {
       path: "../public/fonts/ChandlerMountain-Regular-iF66cbd28d33c0a.ttf",
       weight: "400",
       style: "normal",
     },
     {
+      path: "../public/fonts/ChandlerMountain-Slanted-iF66cbd28d68038.ttf",
+      weight: "400",
+      style: "italic",
+    },
+    {
+      path: "../public/fonts/ChandlerMountain-Medium-iF66cbd244c9f8f.ttf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/ChandlerMountain-MedSlant-iF66cbd244dde22.ttf",
+      weight: "500",
+      style: "italic",
+    },
+    {
+      path: "../public/fonts/ChandlerMountain-SemiBold-iF66cbd28d5953d.ttf",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/ChandlerMountain-SemBdSlant-iF66cbd28d4a187.ttf",
+      weight: "600",
+      style: "italic",
+    },
+    {
       path: "../public/fonts/ChandlerMountain-Bold-iF66cbd18dbcc36.ttf",
       weight: "700",
       style: "normal",
+    },
+    {
+      path: "../public/fonts/ChandlerMountain-BoldSlanted-iF66cbd18dd1f38.ttf",
+      weight: "700",
+      style: "italic",
+    },
+    {
+      path: "../public/fonts/ChandlerMountain-ExtraBold-iF66cbd1d2b1d20.ttf",
+      weight: "800",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/ChandlerMountain-ExtBdSlant-iF66cbd1d28bd66.ttf",
+      weight: "800",
+      style: "italic",
+    },
+    {
+      path: "../public/fonts/ChandlerMountain-Black-iF66cbd18d98284.ttf",
+      weight: "900",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/ChandlerMountain-BlackSlanted-iF66cbd18da884f.ttf",
+      weight: "900",
+      style: "italic",
     },
   ],
   variable: "--font-heading",
